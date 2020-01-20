291646e68a52895480a4aff18790bd4a9b44d33e
Andraz Cuderman <cuderman.andraz@gmail.com>
2020-01-20 23:19:55 +0100

    small refactor

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -10,16 +10,10 @@ import (
 	"math/big"
 )
 
-// Take the data from the block
-
-// create a counter (nonce) which starts at 0
-
-// create a hash of the data plus the counter
-
-// check the hash to see if it meets a set of requirements
-
-// Requirements:
-// The First few bytes must contain 0s
+/*
+Requirements:
+Find nonce which causes first few bytes to contain 0s
+*/
 
 const Difficulty = 18
 
--- main.go
+++ main.go
@@ -9,14 +9,13 @@ import (
 func main() {
 	chain := blockchain.InitBlockChain()
 
-	chain.AddBlock("First Block after Genesis")
-	chain.AddBlock("Second Block after Genesis")
-	chain.AddBlock("Third Block after Genesis")
+	chain.AddBlock("First")
+	chain.AddBlock("Second")
 
 	for _, block := range chain.Blocks {
 
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
 
 		pow := blockchain.NewProof(block)
